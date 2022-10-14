313dc3ce5190be99089240fa7d766a23b28ed3f4
lhqezio <94709107+lhqezio@users.noreply.github.com>
2022-10-14 14:56:07 -0400

    Change blur to focus out

--- js/main_program.js
+++ js/main_program.js
@@ -13,6 +13,7 @@ let projArr = {};
 window.addEventListener('DOMContentLoaded', init);
 function init() {
     document.getElementById('submit').disabled = true;
-    document.getElementById('input-section').addEventListener('input', validateRouter);
+    //Use focusout as a trigger to validate the field as blur does not support bubbling
+    document.getElementById('input-section').addEventListener('focusout', validateRouter);
 
 }
