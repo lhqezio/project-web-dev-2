9b546ed26546f452553ce759ed2ebbd02ab71f72
lhqezio <94709107+lhqezio@users.noreply.github.com>
2022-10-14 14:22:14 -0400

    Use event target

--- js/main_program.js
+++ js/main_program.js
@@ -13,12 +13,6 @@ let projArr = {};
 window.addEventListener('DOMContentLoaded', init);
 function init() {
     document.getElementById('submit').disabled = true;
-    document.getElementById('project-id').addEventListener('blur', validateProjectId);
-    document.getElementById('owner').addEventListener('blur', validateOwner);
-    document.getElementById('title').addEventListener('blur', validateTitle);
-    document.getElementById('category').addEventListener('blur', validateCategory);
-    document.getElementById('hours').addEventListener('blur', validateHours);
-    document.getElementById('rate').addEventListener('blur', validateRate);
-    document.getElementById('description').addEventListener('blur', validateDescription);
-    document.getElementById('status').addEventListener('blur', validateStatus);
+    document.getElementById('input-section').addEventListener('input', validateRouter);
+
 }
--- js/utilities.js
+++ js/utilities.js
@@ -1,54 +1,94 @@
-function validateProjectId() {
-    let value = document.getElementById('project-id').value;
+function validateRouter(ev){
+    console.log(`ran validateRouter`);
+    let event = ev.target;
+    let id = event.id;
+    console.log(`id: ${id}`);
+    switch(id){
+        case 'project-id':
+            validateProjectId(event);
+            break;
+        case 'owner':
+            validateOwner(event);
+            break;
+        case 'title':
+            validateTitle(event);
+            break;
+        case 'category':
+            validateCategory(event);
+            break;
+        case 'hours':
+            validateHours(event);
+            break;
+        case 'rate':
+            validateRate(event);
+            break;
+        case 'description':
+            validateDescription(event);
+            break;
+        case 'status':
+            validateStatus(event);
+            break;
+    }
+}
+function validateProjectId(event) {
+    let value = event.value;
     const PATTERN = /^[A-Za-z]([a-zA-Z0-9-_$]){2,9}$/;
     field_valid['project-id'] = PATTERN.test(value);
+    console.log(field_valid['project-id']);
     buttonEnb()
     return field_valid['project-id'];
 }
-function validateOwner() {
-    let value = document.getElementById('owner').value;
+function validateOwner(event) {
+    let value = event.value;
     const PATTERN = /^[A-Za-z]([a-zA-Z0-9-]){2,9}$/;
     field_valid['owner'] = PATTERN.test(value);
+    console.log(field_valid['owner']);
     buttonEnb()
     return field_valid['owner'];
 }
-function validateTitle() {
-    let value = document.getElementById('title').value;
+function validateTitle(event) {
+    let value = event.value;
     const PATTERN = /^[A-Za-z]{3,25}$/;
     field_valid['title'] = PATTERN.test(value);
+    console.log(field_valid['title']);
     buttonEnb()
     return field_valid['title'];
 }
-function validateCategory() {
-    let value = document.getElementById('category').value;
+function validateCategory(event) {
+    let value = event.value;
     field_valid['category'] = value != null;
+    console.log(field_valid['category']);
     buttonEnb()
     return field_valid['category'];
 }
-function validateHours() {
-    let value = document.getElementById('hours').value;
+function validateHours(event) {
+    let value = event.value;
     const PATTERN = /^[0-9]{1,3}$/;
     field_valid['hours'] = PATTERN.test(value);
+    console.log(field_valid['hours']);
     buttonEnb()
     return field_valid['hours'];
 }
-function validateRate() {
-    let value = document.getElementById('rate').value;
+function validateRate(event) {
+    let value = event.value;
     const PATTERN = /^[0-9]{1,3}$/;
     field_valid['rate'] = PATTERN.test(value);
+    console.log(field_valid['rate']);
     buttonEnb()
     return field_valid['rate'];
 }
-function validateDescription() {
-    let value = document.getElementById('description').value;
+function validateDescription(event) {
+    let value = event.value;
     const PATTERN = /^[A-Za-z]{3,25}$/;
     field_valid['description'] = PATTERN.test(value);
+    console.log(field_valid['description']);
     buttonEnb()
     return field_valid['description'];
 }
-function validateStatus() {
-    let value = document.getElementById('status').value;
+function validateStatus(event) {
+    let value = event.value;
     field_valid['status'] = value != null;
+    console.log(field_valid['status']);
     buttonEnb()
     return field_valid['status'];
 }
@@ -61,5 +101,6 @@ function buttonEnb(){
             break;
         }
     }
+    console.log(`Is button enabled? ${valid}`);
     button.disabled = !valid;
 }
