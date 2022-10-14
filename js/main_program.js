db24683c23dc8a50c9146674357d9be338526b90
lhqezio <94709107+lhqezio@users.noreply.github.com>
2022-10-14 13:29:38 -0400

    Fix JS and Add Button Enabler

--- js/main_program.js
+++ js/main_program.js
@@ -1,6 +1,5 @@
-import * as utils from './utilities.js';
-import * as mp from './manage_projects.js';
-var field_valid = {
+'use strict'
+let field_valid = {
     'project_id': false,
     owner: false,
     title: false,
@@ -10,7 +9,16 @@ var field_valid = {
     description: false,
     status: false,
 }
+let projArr = {};
 window.addEventListener('DOMContentLoaded', init);
 function init() {
-
+    document.getElementById('submit').disabled = true;
+    document.getElementById('project-id').addEventListener('blur', validateProjectId);
+    document.getElementById('owner').addEventListener('blur', validateOwner);
+    document.getElementById('title').addEventListener('blur', validateTitle);
+    document.getElementById('category').addEventListener('blur', validateCategory);
+    document.getElementById('hours').addEventListener('blur', validateHours);
+    document.getElementById('rate').addEventListener('blur', validateRate);
+    document.getElementById('description').addEventListener('blur', validateDescription);
+    document.getElementById('status').addEventListener('blur', validateStatus);
 }
--- js/utilities.js
+++ js/utilities.js
@@ -1,41 +1,65 @@
-export function validateProjectId() {
+function validateProjectId() {
     let value = document.getElementById('project-id').value;
-    const PATTERN = /^[A-Za-z]([a-zA-Z0-9-_$]){2,9}/;
+    const PATTERN = /^[A-Za-z]([a-zA-Z0-9-_$]){2,9}$/;
     field_valid['project-id'] = PATTERN.test(value);
+    buttonEnb()
     return field_valid['project-id'];
 }
-export function validateOwner() {
+function validateOwner() {
     let value = document.getElementById('owner').value;
-    const PATTERN = /^[A-Za-z]([a-zA-Z0-9-]){2,9}/;
+    const PATTERN = /^[A-Za-z]([a-zA-Z0-9-]){2,9}$/;
     field_valid['owner'] = PATTERN.test(value);
+    buttonEnb()
     return field_valid['owner'];
 }
-export function validateTitle() {
+function validateTitle() {
     let value = document.getElementById('title').value;
-    const PATTERN = /^[A-Za-z]{3,25}/;
+    const PATTERN = /^[A-Za-z]{3,25}$/;
     field_valid['title'] = PATTERN.test(value);
+    buttonEnb()
     return field_valid['title'];
 }
-export function validateCategory() {
+function validateCategory() {
     let value = document.getElementById('category').value;
     field_valid['category'] = value != null;
+    buttonEnb()
     return field_valid['category'];
 }
-export function validateHours() {
+function validateHours() {
     let value = document.getElementById('hours').value;
-    const PATTERN = /^[0-9]{1,3}/;
+    const PATTERN = /^[0-9]{1,3}$/;
     field_valid['hours'] = PATTERN.test(value);
+    buttonEnb()
     return field_valid['hours'];
 }
-export function validateRate() {
+function validateRate() {
     let value = document.getElementById('rate').value;
-    const PATTERN = /^[0-9]{1,3}/;
+    const PATTERN = /^[0-9]{1,3}$/;
     field_valid['rate'] = PATTERN.test(value);
+    buttonEnb()
     return field_valid['rate'];
 }
-export function validateDescription() {
+function validateDescription() {
     let value = document.getElementById('description').value;
-    const PATTERN = /^[A-Za-z]{3,25}/;
+    const PATTERN = /^[A-Za-z]{3,25}$/;
     field_valid['description'] = PATTERN.test(value);
+    buttonEnb()
     return field_valid['description'];
+}
+function validateStatus() {
+    let value = document.getElementById('status').value;
+    field_valid['status'] = value != null;
+    buttonEnb()
+    return field_valid['status'];
+}
+function buttonEnb(){
+    let button = document.getElementById('submit');
+    let valid = true;
+    for (let key in field_valid) {
+        if (field_valid[key] === false) {
+            valid = false;
+            break;
+        }
+    }
+    button.disabled = !valid;
 }
