6cf9e75bcc8f4d52b3889d22e2930476e6714e0e
Hoang Quoc <lhqezio1010@gmail.com>
2022-10-14 21:58:58 -0400

    BUG AT LINE 55 manage_projects.js

--- js/manage_projects.js
+++ js/manage_projects.js
@@ -10,5 +10,56 @@ function addProject() {
         status: document.getElementById('status').value,
     }
     projArr.push(project);
+    disableButton();
+    projRender();
     clearFields();
+}
+function projRender() {
+    let projTable = document.createElement('tbody');
+
+    projArr.forEach(tableCreator)
+    function tableCreator(elem,i){
+        let row = document.createElement('tr');
+        let cell = document.createElement('td');
+        let editImg = document.createElement('img');
+        let trashImg = document.createElement('img');
+        editImg.src = '../images/edit.png';
+        editImg.setAttribute('id',`e${i}` );
+        editImg.setAttribute('class','edit');
+        trashImg.src = '../images/trash.png';
+        trashImg.setAttribute('id',`t${i}` );
+        trashImg.setAttribute('class','trash');
+        cell.innerHTML = elem.id;
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.innerHTML = elem.owner;
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.innerHTML = elem.title;
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.innerHTML = elem.category;
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.innerHTML = elem.status;
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.innerHTML = elem.hours;
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.innerHTML = elem.rate;
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.innerHTML = elem.description;
+        row.appendChild(cell);
+        //BUG HERE IMAGE WOULD NOT SHOW
+        cell = document.createElement('td');
+        cell.appendChild(editImg);
+        row.appendChild(cell);
+        cell = document.createElement('td');
+        cell.appendChild(trashImg);
+        row.appendChild(cell);
+        projTable.appendChild(row);
+    }
+    document.getElementById('proj-table').replaceChild(projTable, document.getElementById('proj-table').lastChild);
 }
