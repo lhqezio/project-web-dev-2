6fce983c4ca5aed5779d31d9a7625f5fe53774b8
Marko <markolitovchenko24@gmail.com>
2022-10-17 08:02:50 -0400

    Fixed validation on page refresh

--- js/utilities.js
+++ js/utilities.js
@@ -1,48 +1,32 @@
 `use strict`;
-let field_valid = getFieldValid();
+
 function validateRouter(ev){
     let event = ev.target;
     let id = event.id;
     switch(id){
         case 'id':
             validateProjectId(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
         case 'owner':
             validateOwner(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
         case 'title':
             validateTitle(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
         case 'category':
             validateCategory(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
         case 'hours':
             validateHours(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
         case 'rate':
             validateRate(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
         case 'status':
             validateStatus(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
         case 'description':
             validateDescription(event);
-            buttonEnb();
-            showErrorMessageIfInvalid(event);
             break;
     }
 }
@@ -50,9 +34,9 @@ function validateRouter(ev){
 function showErrorMessageIfInvalid(event){
     let id = event.id;
     let errorMsg = document.getElementById(id+"-error");
-    if (field_valid[`${id}`] && errorMsg !== null){
+    if (fieldValid[`${id}`] && errorMsg !== null){
         event.parentElement.parentElement.removeChild(errorMsg);
-    } else if (!field_valid[`${id}`] && errorMsg === null){
+    } else if (!fieldValid[`${id}`] && errorMsg === null){
         errorMsg = document.createElement("div");
         errorMsg.classList += "error-message";
         errorMsg.setAttribute("id", id+"-error");
@@ -66,61 +50,77 @@ function showErrorMessageIfInvalid(event){
         event.parentElement.appendChild(image);
         image.setAttribute("id", `${id}-img`);
     }
-    image.setAttribute("src", field_valid[`${id}`] ? "../images/valid.png" : "../images/invalid.png");
+    image.setAttribute("src", fieldValid[`${id}`] ? "../images/valid.png" : "../images/invalid.png");
 }
 
 function validateProjectId(event,id=event.id) {
     let value = event.value;
     const PATTERN = /^[A-Za-z]([a-zA-Z0-9-_$]){2,9}$/;
-    field_valid[`${id}`] = PATTERN.test(value);
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = PATTERN.test(value);
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function validateOwner(event,id=event.id) {
     let value = event.value;
     const PATTERN = /^[A-Za-z]([a-zA-Z0-9-]){2,9}$/;
-    field_valid[`${id}`] = PATTERN.test(value);
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = PATTERN.test(value);
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function validateTitle(event,id=event.id) {
     let value = event.value;
     const PATTERN = /^[A-Za-z]{3,25}$/;
-    field_valid[`${id}`] = PATTERN.test(value);
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = PATTERN.test(value);
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function validateCategory(event,id=event.id) {
     let value = event.value;
-    field_valid[`${id}`] = value !== "null";
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = value !== "null";
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function validateHours(event,id=event.id) {
     let value = event.value;
     const PATTERN = /^[0-9]{1,3}$/;
-    field_valid[`${id}`] = PATTERN.test(value);
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = PATTERN.test(value);
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function validateRate(event,id=event.id) {
     let value = event.value;
     const PATTERN = /^[0-9]{1,3}$/;
-    field_valid[`${id}`] = PATTERN.test(value);
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = PATTERN.test(value);
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function validateDescription(event,id=event.id) {
     let value = event.value;
     const PATTERN = /^[A-Za-z][A-Za-z ]{2,24}$/;
-    field_valid[`${id}`] = PATTERN.test(value);
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = PATTERN.test(value);
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function validateStatus(event,id=event.id) {
     let value = event.value;
-    field_valid[`${id}`] = value !== "null";
-    return field_valid[`${id}`];
+    fieldValid[`${id}`] = value !== "null";
+    showErrorMessageIfInvalid(event);
+    buttonEnb();
+    return fieldValid[`${id}`];
 }
 function buttonEnb(){
     let button = document.getElementById('submit');
     let valid = true;
-    for (let key in field_valid) {
-        //console.log(`${field_valid[key]} - ${key}`);
-        if (field_valid[key] === false) {
+    for (let key in fieldValid) {
+        //console.log(`${fieldValid[key]} - ${key}`);
+        if (fieldValid[key] === false) {
             valid = false;
             break;
         }
@@ -153,20 +153,19 @@ function clearAllErrorMessages(){
     let status = document.getElementById("status");
     let description = document.getElementById("description");
 
-    
-    validateProjectId(id);
-    validateOwner(owner);
-    validateTitle(title);
-    validateCategory(category);
-    validateHours(hours);
-    validateRate(rate);
-    validateStatus(status);
-    validateDescription(description);
+    if (id.value !== "")   validateProjectId(id);
+    if (owner.value !== "") validateOwner(owner);
+    if (title.value !== "")validateTitle(title);
+    if (category.value !== "null")validateCategory(category);
+    if (hours.value != "")validateHours(hours);
+    if (rate.value != "")validateRate(rate);
+    if (status.value !== "null")validateStatus(status);
+    if (description.value !== "")validateDescription(description);
 }
 
 function resetAllFields(){
-    for (let key in field_valid) {
-        field_valid[key] = false;
+    for (let key in fieldValid) {
+        fieldValid[key] = false;
     }
 }
 //To clear all input fields and error messages when add button is clicked
@@ -252,8 +251,8 @@ function saveEdit(id){
     validateTitle(document.getElementById("edit-title"), "title");
     validateCategory(document.getElementById("edit-category"), "category");
     let valid = true;
-    for (let key in field_valid) {
-        if (field_valid[key] === false) {
+    for (let key in fieldValid) {
+        if (fieldValid[key] === false) {
             valid = false;
             break;
         }
@@ -318,8 +317,6 @@ function searchByKeyword(){
     else {
         projArr.filter((obj, i) => {
             for (let value of Object.values(obj)){
-    
-                //console.log(`${value} : ${keyWord}`);
                 if (String(value) === keyWord){
                     indexArr.push(+i);
                     break;
